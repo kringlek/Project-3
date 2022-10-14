1db7e138bf60ab43e4126026e2919e4009356348
apfleegor <apfleegor@oxy.edu>
2022-10-13 20:18:56 -0400

    names of hurricanes now as layers

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,81 +12,33 @@ var baseMaps
 //     // createMap(data)
 // });
 
-// function createMap(hurricanes) {
+function create_path (coords) {
+    line = coords;
+    options = {
+        color: "black"
+    }
+    return L.polyline(line, options);
+};
+
 function createMap(hurricane) {
-
-    function create_path (coords) {
-        line = coords;
-        options = {
-            color: "black"
-        }
-        return L.polyline(line, options);
-    };
-
-    // hurr_names = Object.keys(hurricanes);
-    // // console.log("working");
-    // for (var i = 0; i < hurr_names.length; i++) {
-    //     coordinates = hurricanes[hurr_names[i]].coords;
-    //     hurr_paths.push(create_path(coordinates));
-    // };
 
     hurr_paths = [];
 
     coordinates = path[hurricane].coords;
     hurr_paths.push(create_path(coordinates));
 
-    // layerControl.removeLayer(hurricanes);
-    // myMap.removeLayer("Hurricanes");
-
-
-    // var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    // });
-
-    // var baseMaps = {
-    //     "Street Map": street,
-    // };
-    // layerControl.removeLayer(overlayMaps);
 
     hurricanes = L.layerGroup(hurr_paths);
 
-    layerControl.addOverlay(hurricanes, "Hurricanes");
+    layerControl.addOverlay(hurricanes, hurricane);
 
-    // overlayMaps = {
-    //     Hurricanes: hurricanes
-    // };
 
-    // L.control.layers(baseMaps, overlayMaps, {
-    //     collapsed: false
-    // }).addTo(myMap);
-
-    // myMap.addLayer(overlayMaps);
-
-    // create map with layers "on" when first loaded
-    // var myMap = L.map("map", {
-    //     center: [
-    //     28, -60
-    //     ],
-    //     zoom: 2.5,
-    //     layers: [street, hurricanes]
-    // });
-
-    // L.control.layers(baseMaps, overlayMaps, {
-    //     collapsed: false
-    // }).addTo(myMap);
 };
 
 function createInitMap() {
-    function create_path (coords) {
-        line = coords;
-        options = {
-            color: "black"
-        }
-        return L.polyline(line, options);
-    };
 
     coordinates = path['Janet 1955'].coords;
-    console.log(coordinates);
+    // console.log(coordinates);
     hurr_paths.push(create_path(coordinates));
 
 
@@ -101,7 +53,7 @@ function createInitMap() {
     var hurricanes = L.layerGroup(hurr_paths);
 
     var overlayMaps = {
-        Hurricanes: hurricanes
+        "Janet 1955": hurricanes
     };
 
     // create map with layers "on" when first loaded
@@ -117,4 +69,4 @@ function createInitMap() {
     layerControl = L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
-}
+};
