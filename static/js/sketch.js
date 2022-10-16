59f6708f1a16a15c5ae6a95525015d5de476e370
Prathibha Shankar <prathibha02@gmail.com>
2022-10-16 11:48:44 -0400

    trying to place the play button

--- static/js/sketch.js
+++ static/js/sketch.js
@@ -7,19 +7,21 @@ var button;
 var amp;
 
 function setup() {
-   createCanvas(200, 100);
+   canvas = createCanvas(75, 45);
    song = loadSound('./../Data/hurricane.mp3', loaded);
    amp = new p5.Amplitude();
-   background(51);
+   background(192,192,192);
+   
+   canvas.parent('sketch-holder')
 }
-
 function loaded() {
-  button = createButton('play');
-  button.mousePressed(togglePlaying);
-}
+  btn2 = createButton('Play');
+  btn2.mousePressed(togglePlaying);
+  }
+
 
 function draw() {
-    background(51);
+    background(128,128,128);
   
     var vol = amp.getLevel();
     var diam = map(vol, 0, 0.3, 10, 200);
@@ -32,9 +34,9 @@ function togglePlaying() {
   if (!song.isPlaying()) {
     song.play();
     song.setVolume(0.3);
-    button.html('pause');
+    btn2.html('Pause');
   } else {
     song.stop();
-    button.html('play');
+    btn2.html('Play');
   }
 }
